627e1586543affdd00735a1e4e75f3b7556653a4
Craig Watt <87949406+CraigWatt@users.noreply.github.com>
2023-01-21 12:10:46 +0000

    Revert "navbar responsiveness in progress"
    This reverts commit 2513e37f7f6565586a6da1cf3db25ae791c8526e.

--- src/components/common/SiteLayout/SiteLayout.tsx
+++ src/components/common/SiteLayout/SiteLayout.tsx
@@ -11,43 +11,31 @@ export interface SiteLayoutProps {
 
 
 const SiteLayout: React.FC<SiteLayoutProps> = ({children}) => {
-
-  const collapseItems = [
-    "Home",
-    "Portfolio",
-    "Blog",
-  ];
-
   return (
     <>
     <div>I am Site Layout Component</div>
-      <Navbar isBordered variant={"sticky"}>
+      <Navbar isBordered variant={"static"}>
           <Navbar.Brand>
-            <Navbar.Toggle aria-label="toggle navigation" showIn="xs" />
             <Text b color="inherit" hideIn="xs">
               ACME
             </Text>
           </Navbar.Brand>
           <Navbar.Content hideIn="xs">
-            <Navbar.Link isActive href="/">Home</Navbar.Link>
-            <Navbar.Link href="#">Portfolio</Navbar.Link>
-            <Navbar.Link href="#">Blog</Navbar.Link>
+            <Navbar.Link href="#">Features</Navbar.Link>
+            <Navbar.Link isActive href="#">Customers</Navbar.Link>
+            <Navbar.Link href="#">Pricing</Navbar.Link>
+            <Navbar.Link href="#">Company</Navbar.Link>
           </Navbar.Content>
-          <Navbar.Collapse>
-            {collapseItems.map((item, index) => (
-            <Navbar.CollapseItem key={item}>
-              <Link
-                color="inherit"
-                css={{
-                  minWidth: "100%",
-                }}
-                href="#"
-              >
-                {item}
-              </Link>
-            </Navbar.CollapseItem>
-            ))}
-          </Navbar.Collapse>
+          <Navbar.Content>
+            <Navbar.Link color="inherit" href="#">
+              Login
+            </Navbar.Link>
+            <Navbar.Item>
+              <Button auto flat as={Link} href="#">
+                Sign Up
+              </Button>
+            </Navbar.Item>
+          </Navbar.Content>
         </Navbar>   
       <main>{children}</main>
     </>
