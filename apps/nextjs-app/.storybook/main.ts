a14d44b469e264dc9b542d068b50c00a9030bc93
Craig Watt <craigwatt@Craigs-MacBook-Air.local>
2025-02-23 15:28:44 +0000

    stage1

--- apps/nextjs-app/.storybook/main.ts
+++ apps/nextjs-app/.storybook/main.ts
@@ -2,11 +2,22 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
+  ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 
 export default config;
