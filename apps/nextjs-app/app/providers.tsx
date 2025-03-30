fc081eef3398329f18060423ac2adab86f6c3616
Craig Watt <craigwatt@192.168.1.115>
2025-03-30 23:03:57 +0100

    ThemeSwitcher now present

--- apps/nextjs-app/app/providers.tsx
+++ apps/nextjs-app/app/providers.tsx
@@ -1,6 +1,15 @@
 // app/providers.tsx
+'use client';
+
 import { HeroUIProvider } from '@heroui/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <HeroUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </NextThemesProvider>
+    </HeroUIProvider>
+  );
 }
