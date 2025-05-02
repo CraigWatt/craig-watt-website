bf5606cbdf13942c8168326070bd6258143e3008
Craig Watt <craigwatt@macbookair.home>
2025-05-02 14:37:19 +0100

    theme switcher revision

--- apps/nextjs-app/app/components/icons/Activity.tsx
+++ apps/nextjs-app/app/components/icons/Activity.tsx
@@ -24,6 +24,16 @@ export const Activity = ({
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    {/* SVG paths here */}
+    <g
+      stroke={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={1.5}
+    >
+      <path d="M6.918 14.854l2.993-3.889 3.414 2.68 2.929-3.78" />
+      <path d="M19.668 2.35a1.922 1.922 0 11-1.922 1.922 1.921 1.921 0 011.922-1.922z" />
+      <path d="M20.756 9.269a20.809 20.809 0 01.194 3.034c0 6.938-2.312 9.25-9.25 9.25s-9.25-2.312-9.25-9.25 2.313-9.25 9.25-9.25a20.931 20.931 0 012.983.187" />
+    </g>
   </svg>
 );
