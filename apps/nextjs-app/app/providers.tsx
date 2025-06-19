ed91e20bab040885acf42e1c85f5cf37771e69b6
CraigWatt <87949406+CraigWatt@users.noreply.github.com>
2025-06-19 22:00:37 +0100

    error and warning fixes

--- apps/nextjs-app/app/providers.tsx
+++ apps/nextjs-app/app/providers.tsx
@@ -1,15 +1,15 @@
 // app/providers.tsx
 'use client';
 
-import { HeroUIProvider } from '@heroui/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { HeroUIProvider } from '@heroui/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-      </NextThemesProvider>
-    </HeroUIProvider>
+    // toggles class="dark" on <html>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      {/* HeroUIProvider just needs to wrap children */}
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </NextThemesProvider>
   );
 }
