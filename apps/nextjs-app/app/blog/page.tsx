039711bd114fe1f24abb517c4a1d9504139306e3
Craig Watt <87949406+CraigWatt@users.noreply.github.com>
2025-06-28 20:04:50 +0100

    again'

--- apps/nextjs-app/app/blog/page.tsx
+++ apps/nextjs-app/app/blog/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { useSearchParams, useRouter } from 'next/navigation';
 import { SiteBreadcrumbs } from '../components/SiteBreadcrumbs';
