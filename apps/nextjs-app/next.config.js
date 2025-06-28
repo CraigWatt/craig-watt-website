4a97c3f7c41fa73836b60526bd4cfaedf227d34c
Craig Watt <87949406+CraigWatt@users.noreply.github.com>
2025-06-28 21:22:16 +0100

    allow standalone

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -4,6 +4,9 @@ import { composePlugins, withNx } from '@nx/next';
 
 /** @type {import('@nx/next/plugins/with-nx').WithNxOptions} */
 const nextConfig = {
+  // Enable standalone output to support Docker multi-stage build
+  output: 'standalone',
+
   nx: {
     // Set this to true if you would like to use SVGR
     svgr: false,
