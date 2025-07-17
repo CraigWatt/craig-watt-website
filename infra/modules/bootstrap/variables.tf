734742a60231e00ba120623ccc7232f9371601b7
Craig Watt <87949406+CraigWatt@users.noreply.github.com>
2025-07-17 22:41:37 +0100

    a

--- infra/modules/bootstrap/variables.tf
+++ infra/modules/bootstrap/variables.tf
@@ -1,24 +1,21 @@
+###### modules/bootstrap/variables.tf ######
+
 variable "state_bucket_name" {
-  type        = string
   description = "Name of the S3 bucket for Terraform state"
+  type        = string
 }
 
 variable "lock_table_name" {
-  type        = string
   description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
 }
 
 variable "github_oidc_provider_url" {
-  type        = string
   description = "URL of the GitHub Actions OIDC provider"
+  type        = string
 }
 
 variable "github_repo" {
+  description = "GitHub repo in Owner/Name format (used in trust policy)"
   type        = string
-  description = "GitHub repo in form Owner/Name"
-}
-
-# derive a short modulename for IAM role naming
-locals {
-  github_repo_short = replace(var.github_repo, "/", "-")
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,23 @@
+variable "tf_state_bucket" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "tf_state_lock_table" {
+  description = "DynamoDB table name for state locking"
+  type        = string
+}
+
+variable "github_oidc_provider_url" {
+  description = "URL of the GitHub Actions OIDC provider"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repo in Owner/Name format (used for OIDC trust policy)"
+  type        = string
+}
+
 variable "aws_region" {
   description = "AWS region to deploy into"
   type        = string
@@ -35,29 +55,4 @@ variable "ecs_execution_role_arn" {
 variable "ecs_task_role_arn" {
   description = "IAM role ARN for your ECS tasks (e.g. ecsTaskRole)"
   type        = string
-}
-
-
-variable "tf_state_bucket" {
-  description = "S3 bucket for Terraform state"
-  type        = string
-  default     = "craig-watt-tfstate"
-}
-
-variable "tf_state_lock_table" {
-  description = "DynamoDB table for Terraform state locking"
-  type        = string
-  default     = "craig-watt-lock-table"
-}
-
-variable "github_oidc_provider_url" {
-  description = "GitHub Actions OIDC provider URL"
-  type        = string
-  default     = "https://token.actions.githubusercontent.com"
-}
-
-variable "github_repo" {
-  description = "GitHub repo in Owner/Name format (for the trust policy)"
-  type        = string
-  default     = "CraigWatt/craig-watt-website"
 }
