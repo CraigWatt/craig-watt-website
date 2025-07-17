b7bfacc7543a519b038a5af02086b55cdecf0667
Craig Watt <craigwatt@macbookair.home>
2025-07-17 20:17:29 +0100

    terraform fixes

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,24 +1,21 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-# Discover your default VPC and its public subnets + default SG
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "public" {
+data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
 }
 
 data "aws_security_group" "default" {
-  name   = "default"
-  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
---
+++ infra/terraform.tfvars
@@ -0,0 +1,3 @@
+ecs_execution_role_arn = "arn:aws:iam::275578070934:role/ecsTaskExecutionRole"
+ecs_task_role_arn      = "arn:aws:iam::275578070934:role/ecsTaskRole"
+image_tag              = "v0.6.10"
