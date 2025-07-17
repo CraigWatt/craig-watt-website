71f13204358e225427f311340531bae3c6b68eb9
CraigWatt <87949406+CraigWatt@users.noreply.github.com>
2025-07-17 20:17:29 +0100

    terraform fixes

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,7 +1,9 @@
 resource "aws_ecr_repository" "this" {
-  name = var.repository_name
-
+  name                 = var.repository_name
   image_scanning_configuration {
     scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "AES256"
   }
 }
