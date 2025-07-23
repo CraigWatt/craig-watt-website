18cfaae353363e369199cff753fff27ea31573af
CraigWatt <87949406+CraigWatt@users.noreply.github.com>
2025-07-23 15:13:28 +0100

    npm mindmelt

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -1,3 +1,4 @@
+// apps/nextjs-app/next.config.js
 // @ts-check
 
 import { composePlugins, withNx } from '@nx/next';
@@ -11,40 +12,16 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     svgr: false,
   },
+
   // ⚠️ Skip type-check errors during build to unblock CI temporarily
   typescript: {
     ignoreBuildErrors: true,
   },
+
   // ⚠️ Skip ESLint checks during build to unblock CI temporarily
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-export default composePlugins(...plugins)(nextConfig);
-
-// what things where BEFORE GITHUB ACTIONS BLOCKER
-
-// // @ts-check
-
-// import { composePlugins, withNx } from '@nx/next';
-
-// /** @type {import('@nx/next/plugins/with-nx').WithNxOptions} */
-// const nextConfig = {
-//   nx: {
-//     // Set this to true if you would like to to use SVGR
-//     svgr: false,
-//   },
-// };
-
-// const plugins = [
-//   // Add more Next.js plugins to this list if needed.
-//   withNx,
-// ];
-
-// export default composePlugins(...plugins)(nextConfig);
+export default composePlugins(withNx)(nextConfig);
---
+++ apps/nextjs-app/node_modules/framer-motion/dist/es/animation/animators/waapi/index.mjs
@@ -0,0 +1,32 @@
+import { mapEasingToNativeEasing, statsBuffer, activeAnimations } from 'motion-dom';
+
+function startWaapiAnimation(element, valueName, keyframes, { delay = 0, duration = 300, repeat = 0, repeatType = "loop", ease = "easeInOut", times, } = {}) {
+    const keyframeOptions = { [valueName]: keyframes };
+    if (times)
+        keyframeOptions.offset = times;
+    const easing = mapEasingToNativeEasing(ease, duration);
+    /**
+     * If this is an easing array, apply to keyframes, not animation as a whole
+     */
+    if (Array.isArray(easing))
+        keyframeOptions.easing = easing;
+    if (statsBuffer.value) {
+        activeAnimations.waapi++;
+    }
+    const animation = element.animate(keyframeOptions, {
+        delay,
+        duration,
+        easing: !Array.isArray(easing) ? easing : "linear",
+        fill: "both",
+        iterations: repeat + 1,
+        direction: repeatType === "reverse" ? "alternate" : "normal",
+    });
+    if (statsBuffer.value) {
+        animation.finished.finally(() => {
+            activeAnimations.waapi--;
+        });
+    }
+    return animation;
+}
+
+export { startWaapiAnimation };
