8999bbc07ac06a7dc24097f63798a025d9922f81
CraigWatt <87949406+CraigWatt@users.noreply.github.com>
2025-08-05 10:15:03 +0100

    add back egress for app in networks for ecr access

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -57,4 +57,12 @@ resource "aws_security_group" "app" {
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
+
+  # allow all outbound traffic (needed so Fargate can pull from ECR, etc)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
