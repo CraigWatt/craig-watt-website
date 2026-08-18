bfa04a3d29e97a26e06e8a922ca8f2ef17cbc697
CraigWatt <87949406+CraigWatt@users.noreply.github.com>
2026-08-18 20:32:53 +0100

    Default bootstrap Terraform variables for this repo

--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -3,23 +3,28 @@
 variable "state_bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
+  default     = "craig-watt-tfstate"
 }
 
 variable "github_oidc_provider_url" {
   description = "URL of the GitHub Actions OIDC provider"
   type        = string
+  default     = "https://token.actions.githubusercontent.com"
 }
 
 variable "github_repo" {
   description = "GitHub repo in Owner/Name format (used in trust policy)"
   type        = string
+  default     = "CraigWatt/craig-watt-website"
 }
 
 variable "aws_region" {
   type = string
+  default = "eu-west-2"
 }
 
 variable "domain" {
   description = "Primary website domain used to derive resource names"
   type        = string
+  default     = "craigwatt.co.uk"
 }
